3856c961ef2dcced2460846eb6233dff184ea4ba
uflBufl <43928374+uflBufl@users.noreply.github.com>
2018-11-22 00:19:36 +0400

    Split into components

--- app.js
+++ app.js
@@ -12,19 +12,184 @@ async function request(url, options) {
   }
 }
 
-class App extends React.Component {
+function LogoFunction(props) {
+  return React.createElement("img", {
+    src: "img/w-mercury-development.svg",
+    className: "logo"
+  });
+}
+
+function LogoutImgFunction(props) {
+  return React.createElement("img", {
+    src: props.user.photoUrl,
+    className: "block__img",
+    id: "photo"
+  });
+}
+
+function LogoutHeadFunction(props) {
+  return React.createElement("h1", {
+    className: "block__headline block__headline_name",
+    id: "UsName"
+  }, props.user.name);
+}
+
+class LogoutHeadClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement("h1", {
+      className: "block__headline block__headline_name",
+      id: "UsName"
+    }, this.props.user.name);
+  }
+
+}
+
+class LogoutButtonClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement("input", {
+      type: "submit",
+      className: "form form__button form__button_profile",
+      value: "Logout"
+    });
+  }
+
+}
+
+class LoginButtonClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      IsLock: false
+    };
+  }
+
+  render() {
+    return React.createElement("input", {
+      type: "submit",
+      name: "login_button",
+      id: "login_button",
+      className: "form__button",
+      disabled: this.props.IsLock,
+      value: "Login"
+    });
+  }
+
+}
+
+class ErrorClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement("div", {
+      className: "form__error",
+      id: "error",
+      name: "error",
+      style: !this.props.error == "" ? {
+        display: "inline-block"
+      } : {
+        display: "none"
+      }
+    }, React.createElement("span", {
+      id: "errortext",
+      name: "errortext"
+    }, this.props.error));
+  }
+
+}
+
+class InputPasswordClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: this.props.password,
+      IsLock: false
+    };
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  handleChangePassword(e) {
+    this.props.handleChangePassword(e);
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  render() {
+    return React.createElement("input", {
+      type: "password",
+      name: "password",
+      id: "password",
+      className: "form__input form__input_password",
+      placeholder: "Password",
+      disabled: this.props.IsLock,
+      onChange: this.handleChangePassword,
+      value: this.props.password
+    });
+  }
+
+}
+
+class InputLoginClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "user@example.com",
+      IsLock: this.props.IsLock,
+      IsRed: this.props.IsRed
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+  }
+
+  handleChangeEmail(e) {
+    this.props.handleChangeEmail(e);
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  render() {
+    const errorChange = {
+      borderColor: "#ed4159",
+      color: "#ed4159"
+    };
+    return React.createElement("input", {
+      onClick: this.props.removeRedColor,
+      type: "email",
+      name: "login",
+      id: "login",
+      className: "form__input",
+      placeholder: "E-Mail",
+      onChange: this.handleChangeEmail,
+      value: this.state.email,
+      disabled: this.props.IsLock,
+      style: this.props.IsRed == true ? errorChange : {}
+    });
+  }
+
+}
+
+class LogInClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "user@example.com",
       password: "mercdev",
-      lock: false,
-      red: false,
+      IsLock: false,
+      IsRed: false,
       error: ""
     };
-    this.logout = this.logout.bind(this);
+    this.removeRedColor = this.removeRedColor.bind(this);
     this.logIn = this.logIn.bind(this);
-    this.remove = this.remove.bind(this);
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
     this.handleChangePassword = this.handleChangePassword.bind(this);
   }
@@ -32,7 +197,7 @@ class App extends React.Component {
   async logIn(e) {
     e.preventDefault();
     this.setState({
-      lock: true,
+      IsLock: true,
       error: ""
     });
     var login = this.state.email;
@@ -51,9 +216,7 @@ class App extends React.Component {
         })
       });
       const user = response;
-      this.setState({
-        user
-      });
+      this.props.clickLogin(user);
     } catch (response) {
       if (response.status == 0) {
         this.setState({
@@ -70,7 +233,7 @@ class App extends React.Component {
       if (response.status == 400) {
         this.setState({
           password: "",
-          red: true,
+          IsRed: true,
           error: "E-Mail or password is incorrect"
         });
       } else {
@@ -81,19 +244,13 @@ class App extends React.Component {
     }
 
     this.setState({
-      lock: false
+      IsLock: false
     });
   }
 
-  remove() {
+  removeRedColor() {
     this.setState({
-      red: false
-    });
-  }
-
-  logout() {
-    this.setState({
-      user: null
+      IsRed: false
     });
   }
 
@@ -110,86 +267,83 @@ class App extends React.Component {
   }
 
   render() {
-    const errorChange = {
-      borderColor: "#ed4159",
-      color: "#ed4159"
-    };
-
-    if (this.state.user) {
-      return React.createElement("div", null, React.createElement("img", {
-        src: "img/w-mercury-development.svg",
-        className: "logo"
-      }), React.createElement("div", {
-        className: "block"
-      }, React.createElement("form", {
-        action: "login.html",
-        method: "GET",
-        onSubmit: this.logout
-      }, React.createElement("img", {
-        src: this.state.user.photoUrl,
-        className: "block__img",
-        id: "photo"
-      }), React.createElement("h1", {
-        className: "block__headline block__headline_name",
-        id: "UsName"
-      }, this.state.user.name), React.createElement("input", {
-        type: "submit",
-        onClick: this.logout,
-        className: "form form__button form__button_profile",
-        value: "Logout"
-      }))));
-    }
-
-    return React.createElement("div", null, React.createElement("img", {
-      src: "img/w-mercury-development.svg",
-      className: "logo"
-    }), React.createElement("div", {
+    return React.createElement("div", null, React.createElement(LogoFunction, null), React.createElement("div", {
       className: "block"
     }, React.createElement("h1", {
       className: "block__headline"
     }, "Log In"), React.createElement("form", {
       onSubmit: this.logIn,
       className: "form"
-    }, React.createElement("input", {
-      onClick: this.remove,
-      type: "email",
-      name: "login",
-      id: "login",
-      className: "form__input",
-      placeholder: "E-Mail",
-      onChange: this.handleChangeEmail,
-      value: this.state.email,
-      disabled: this.state.lock,
-      style: this.state.red == true ? errorChange : {}
-    }), React.createElement("input", {
-      type: "password",
-      name: "password",
-      id: "password",
-      className: "form__input form__input_password",
-      placeholder: "Password",
-      disabled: this.state.lock,
-      onChange: this.handleChangePassword,
-      value: this.state.password
-    }), React.createElement("div", {
-      className: "form__error",
-      id: "error",
-      name: "error",
-      style: !this.state.error == "" ? {
-        display: "inline-block"
-      } : {
-        display: "none"
-      }
-    }, React.createElement("span", {
-      id: "errortext",
-      name: "errortext"
-    }, this.state.error)), React.createElement("input", {
-      type: "submit",
-      name: "login_button",
-      id: "login_button",
-      className: "form__button",
-      disabled: this.state.lock,
-      value: "Login"
+    }, React.createElement(InputLoginClass, {
+      removeRedColor: this.removeRedColor,
+      IsRed: this.state.IsRed,
+      IsLock: this.state.IsLock,
+      handleChangeEmail: this.handleChangeEmail
+    }), React.createElement(InputPasswordClass, {
+      password: this.state.password,
+      IsLock: this.state.IsLock,
+      handleChangePassword: this.handleChangePassword
+    }), React.createElement(ErrorClass, {
+      error: this.state.error
+    }), React.createElement(LoginButtonClass, {
+      lock: this.state.IsLock
     }))));
+  }
+
+}
+
+class LogOutClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement("div", null, React.createElement(LogoFunction, null), React.createElement("div", {
+      className: "block"
+    }, React.createElement("form", {
+      action: "login.html",
+      method: "GET",
+      onSubmit: this.props.clickLogout
+    }, React.createElement(LogoutImgFunction, {
+      user: this.props.user
+    }), React.createElement(LogoutHeadFunction, {
+      user: this.props.user
+    }), React.createElement(LogoutButtonClass))));
+  }
+
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.clickLogout = this.clickLogout.bind(this);
+    this.clickLogin = this.clickLogin.bind(this);
+  }
+
+  clickLogin(user) {
+    this.setState({
+      user
+    });
+  }
+
+  clickLogout() {
+    this.setState({
+      user: null
+    });
+  }
+
+  render() {
+    if (this.state.user) {
+      return React.createElement(LogOutClass, {
+        user: this.state.user,
+        clickLogout: this.clickLogout
+      });
+    }
+
+    return React.createElement(LogInClass, {
+      clickLogin: this.clickLogin
+    });
   }
 
 }
--- app.jsx
+++ app.jsx
@@ -11,21 +11,192 @@ async function request(url, options) {
   }
 }
 
-class App extends React.Component {
+function LogoFunction(props) {
+  return <img src="img/w-mercury-development.svg" className="logo" />;
+}
+
+function LogoutImgFunction(props) {
+  return <img src={props.user.photoUrl} className="block__img" id="photo" />;
+}
+
+function LogoutHeadFunction(props) {
+  return (
+    <h1 className="block__headline block__headline_name" id="UsName">
+      {props.user.name}
+    </h1>
+  );
+}
+
+class LogoutHeadClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1 className="block__headline block__headline_name" id="UsName">
+        {this.props.user.name}
+      </h1>
+    );
+  }
+}
+
+class LogoutButtonClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <input
+        type="submit"
+        className="form form__button form__button_profile"
+        value="Logout"
+      />
+    );
+  }
+}
+
+class LoginButtonClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      IsLock: false
+    };
+  }
+
+  render() {
+    return (
+      <input
+        type="submit"
+        name="login_button"
+        id="login_button"
+        className="form__button"
+        disabled={this.props.IsLock}
+        value="Login"
+      />
+    );
+  }
+}
+
+class ErrorClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div
+        className="form__error"
+        id="error"
+        name="error"
+        style={
+          !this.props.error == ""
+            ? { display: "inline-block" }
+            : { display: "none" }
+        }
+      >
+        <span id="errortext" name="errortext">
+          {this.props.error}
+        </span>
+      </div>
+    );
+  }
+}
+
+class InputPasswordClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: this.props.password,
+      IsLock: false
+    };
+
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  handleChangePassword(e) {
+    this.props.handleChangePassword(e);
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <input
+        type="password"
+        name="password"
+        id="password"
+        className="form__input form__input_password"
+        placeholder="Password"
+        disabled={this.props.IsLock}
+        onChange={this.handleChangePassword}
+        value={this.props.password}
+      />
+    );
+  }
+}
+
+class InputLoginClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "user@example.com",
+      IsLock: this.props.IsLock,
+      IsRed: this.props.IsRed
+    };
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+  }
+
+  handleChangeEmail(e) {
+    this.props.handleChangeEmail(e);
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  render() {
+    const errorChange = {
+      borderColor: "#ed4159",
+      color: "#ed4159"
+    };
+
+    return (
+      <input
+        onClick={this.props.removeRedColor}
+        type="email"
+        name="login"
+        id="login"
+        className="form__input"
+        placeholder="E-Mail"
+        onChange={this.handleChangeEmail}
+        value={this.state.email}
+        disabled={this.props.IsLock}
+        style={this.props.IsRed == true ? errorChange : {}}
+      />
+    );
+  }
+}
+
+class LogInClass extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       email: "user@example.com",
       password: "mercdev",
-      lock: false,
-      red: false,
+      IsLock: false,
+      IsRed: false,
       error: ""
     };
 
-    this.logout = this.logout.bind(this);
+    this.removeRedColor = this.removeRedColor.bind(this);
     this.logIn = this.logIn.bind(this);
-    this.remove = this.remove.bind(this);
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
     this.handleChangePassword = this.handleChangePassword.bind(this);
   }
@@ -34,7 +205,7 @@ class App extends React.Component {
     e.preventDefault();
 
     this.setState({
-      lock: true,
+      IsLock: true,
       error: ""
     });
 
@@ -52,9 +223,8 @@ class App extends React.Component {
       });
 
       const user = response;
-      this.setState({
-        user
-      });
+
+      this.props.clickLogin(user);
     } catch (response) {
       if (response.status == 0) {
         this.setState({
@@ -69,7 +239,7 @@ class App extends React.Component {
       if (response.status == 400) {
         this.setState({
           password: "",
-          red: true,
+          IsRed: true,
           error: "E-Mail or password is incorrect"
         });
       } else {
@@ -79,19 +249,13 @@ class App extends React.Component {
       }
     }
     this.setState({
-      lock: false
+      IsLock: false
     });
   }
 
-  remove() {
+  removeRedColor() {
     this.setState({
-      red: false
-    });
-  }
-
-  logout() {
-    this.setState({
-      user: null
+      IsRed: false
     });
   }
 
@@ -108,98 +272,94 @@ class App extends React.Component {
   }
 
   render() {
-    const errorChange = {
-      borderColor: "#ed4159",
-      color: "#ed4159"
-    };
-    
-    if (this.state.user) {
-      return (
-        <div>
-          <img src="img/w-mercury-development.svg" className="logo" />
-
-          <div className="block">
-            <form action="login.html" method="GET" onSubmit={this.logout}>
-              <img
-                src={this.state.user.photoUrl}
-                className="block__img"
-                id="photo"
-              />
-
-              <h1 className="block__headline block__headline_name" id="UsName">
-                {this.state.user.name}
-              </h1>
-
-              <input
-                type="submit"
-                onClick={this.logout}
-                className="form form__button form__button_profile"
-                value="Logout"
-              />
-            </form>
-          </div>
-        </div>
-      );
-    }
-
     return (
       <div>
-        <img src="img/w-mercury-development.svg" className="logo" />
+        <LogoFunction />
 
         <div className="block">
           <h1 className="block__headline">Log In</h1>
           <form onSubmit={this.logIn} className="form">
-            <input
-              onClick={this.remove}
-              type="email"
-              name="login"
-              id="login"
-              className="form__input"
-              placeholder="E-Mail"
-              onChange={this.handleChangeEmail}
-              value={this.state.email}
-              disabled={this.state.lock}
-              style={this.state.red == true ? errorChange : {}}
-            />
+            {React.createElement(InputLoginClass, {
+              removeRedColor: this.removeRedColor,
+              IsRed: this.state.IsRed,
+              IsLock: this.state.IsLock,
+              handleChangeEmail: this.handleChangeEmail
+            })}
 
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="form__input form__input_password"
-              placeholder="Password"
-              disabled={this.state.lock}
-              onChange={this.handleChangePassword}
-              value={this.state.password}
-            />
+            {React.createElement(InputPasswordClass, {
+              password: this.state.password,
+              IsLock: this.state.IsLock,
+              handleChangePassword: this.handleChangePassword
+            })}
 
-            <div
-              className="form__error"
-              id="error"
-              name="error"
-              style={
-                !this.state.error == ""
-                  ? { display: "inline-block" }
-                  : { display: "none" }
-              }
-            >
-              <span id="errortext" name="errortext">
-                {this.state.error}
-              </span>
-            </div>
+            {React.createElement(ErrorClass, { error: this.state.error })}
 
-            <input
-              type="submit"
-              name="login_button"
-              id="login_button"
-              className="form__button"
-              disabled={this.state.lock}
-              value="Login"
-            />
+            {React.createElement(LoginButtonClass, { lock: this.state.IsLock })}
           </form>
         </div>
       </div>
     );
+  }
+}
+
+class LogOutClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <LogoFunction />
+
+        <div className="block">
+          <form
+            action="login.html"
+            method="GET"
+            onSubmit={this.props.clickLogout}
+          >
+            <LogoutImgFunction user={this.props.user} />
+
+            <LogoutHeadFunction user={this.props.user} />
+
+            {React.createElement(LogoutButtonClass)}
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.clickLogout = this.clickLogout.bind(this);
+    this.clickLogin = this.clickLogin.bind(this);
+  }
+
+  clickLogin(user) {
+    this.setState({
+      user
+    });
+  }
+
+  clickLogout() {
+    this.setState({
+      user: null
+    });
+  }
+
+  render() {
+    if (this.state.user) {
+      return React.createElement(LogOutClass, {
+        user: this.state.user,
+        clickLogout: this.clickLogout
+      });
+    }
+    return React.createElement(LogInClass, { clickLogin: this.clickLogin });
   }
 }
 
