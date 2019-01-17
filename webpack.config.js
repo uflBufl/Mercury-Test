ae32b7eb492f9640bab28a0710b1fe0fa55c9e5f
uflBufl <43928374+uflBufl@users.noreply.github.com>
2019-01-17 16:14:18 +0400

    fix mobx

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,8 @@ module.exports = {
                 ]
               ]			,
               "plugins": [
-                  [
-                    "@babel/plugin-proposal-class-properties"
-                  ],
-                  ["@babel/plugin-proposal-decorators", { "legacy": true }]
+                ["@babel/plugin-proposal-decorators", { legacy: true }],
+                ["@babel/plugin-proposal-class-properties", { loose: true }]
               ]					
             }
           },
